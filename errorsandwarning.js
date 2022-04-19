0b216aae21d872a86712d5020b86037d1ec470b5
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2022-04-19 21:37:38 +0530

    all the codes committing at once

---
+++ errorsandwarning.js
@@ -0,0 +1,17 @@
+// console.time("Your code took:");here "Your code took:"is an identifier should be used in timeEnd as well same for it
+console.log("Tut1");
+// console.log can be used as calculator also 
+// console.log(23+2);
+// prints the objects in tabular Form
+// console.table({hello:"dhruv",marks:29})
+// for warning
+// console.warn("warning"); 
+// to clear
+// console.clear();
+// for timing of code
+// console.timeEnd("Your code took:");
+// for any assertion error
+// console.assert(condition,statement if true);
+// console.assert(1<2,"Not possible");//no error
+// console.assert(3<2,"Not possible");//assertion error
+// console.error("this is an error");//direct error
---
+++ windowobject.js
@@ -0,0 +1,87 @@
+console.log("DOM Window objects");
+// let a=window;//window is a global object and this is the way to print all objects
+// alert-gives alert topmost
+// alert("Hello my name is DHhruv");
+
+// prompt-takes a string input 
+// a=prompt("Type your name: ")
+// console.log(a);
+
+//confirm-true or false boolean assigned
+// confirm("Are you sure you want to exit!")
+
+// let b=window.document;//gives html files allocated to it
+// console.log(b);
+
+// c=window.innerHeight;//height of opened window 
+// console.log(c);
+// c=window.innerWidth;//width of opened window
+// console.log(c);
+// can be run without using window. 
+
+// scrollX-prints the scroll in x axis value
+// scrollY-print the scroll in y axis value
+// a=scrollX//by default 0
+// console.log(a);
+
+// alert()
+
+// It will display an alert box with a message on the screen
+
+// blur()
+
+// It will remove focus from the current window
+
+// close()
+
+// It will close the current window
+
+// confirm()
+
+// It will display a dialog box with a message on the screen
+
+// focus()
+
+// It will set the focus to the current window
+
+// moveTo()
+
+// It will move a window to the specified position
+
+// open()
+
+// It will open a new browser window
+
+// print()
+
+// It will print the content of the current window
+
+// prompt()
+
+// It will display a dialog box that prompts the user for input
+
+// resizeBy()
+
+// It will resize the window by the specified pixels
+
+// resizeTo()
+
+// It will resize the window to the specified height and width
+
+// scrollBy()
+
+// It will scroll the document by the specified number of pixels
+
+// scrollTo()
+
+// It will scroll the document to the specified coordinates
+
+// stop()
+
+// It will stop the window from loading
+// a = window.innerHeight;
+// a = innerWidth;
+// a = scrollY;
+// a = location.toString();
+// a = window.history;
+// console.log(a);          
